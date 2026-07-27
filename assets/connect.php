16a16b6b889ac881691fcfa8606b7c851c35dc16
septharie <165117494+septharie@users.noreply.github.com>
2026-07-27 13:31:13 +0700

    Update connect.php

--- assets/connect.php
+++ assets/connect.php
@@ -1,5 +1,5 @@
 <?php
 
-$db = new PDO('mysql:host=localhost; dbname=testing1; charset=utf8mb4', 'root', '');
+$db = new PDO('mysql:host=localhost; dbname=simplecrud; charset=utf8mb4', 'root', '');
 
 ?>
